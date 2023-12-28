8bde39aa9e19848aae372991f424e2b444314967
constoner <constantin.kalinin@gmail.com>
2023-12-28 23:39:50 +0300

    fix href in the Logo

--- src/components/misc/Logo/Logo.jsx
+++ src/components/misc/Logo/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = ({ className }) => {
   return (
     <a
       className={className}
-      href="./"
+      href={process.env.PUBLIC_URL + "/"}
       aria-label="Plant's logo - click to home page."
     >
       <Icon
